c421b741c77ef16f4e5f9fa018edf7ee6c1daef7
Luis A. Heredia Mariñez <l.heredia@unibe.edu.do>
2019-10-15 18:36:52 -0400

    ...

--- app/app/task.aspx.cs
+++ app/app/task.aspx.cs
@@ -4,13 +4,80 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace app
 {
     public partial class task : System.Web.UI.Page
     {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/tasks-dotnet-quickstart.json
+        static string[] Scopes = { TasksService.Scope.TasksReadonly };
+        static string ApplicationName = "Google Tasks API .NET Quickstart";
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Response.Write("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Tasks API service.
+            var service = new TasksService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            TasklistsResource.ListRequest listRequest = service.Tasklists.List();
+            listRequest.MaxResults = 10;
+
+            // List task lists.
+            IList<TaskList> taskLists = listRequest.Execute().Items;
+            Response.Write("Task Lists:");
+            if (taskLists != null && taskLists.Count > 0)
+            {
+                foreach (var taskList in taskLists)
+                {
+                    Response.Write(taskList.Title);
+                    Response.Write("<br />");
+                    Response.Write(taskList.Id);
+                    Response.Write("<br />");
+                }
+            }
+            else
+            {
+                Response.Write("No task lists found.");
+                
+            }
+
 
         }
     }
